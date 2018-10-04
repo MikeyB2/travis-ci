7de0928865546c4561483c3d53b9167b66d18b43
Mike <mike.bryant.jr@icloud.com>
2018-10-03 22:17:07 -0600

    added recipe mock data

--- public/app.js
+++ public/app.js
@@ -49,16 +49,93 @@ function displayShoppingList(data) {
         $('body').append(
             '<li>' + data.shoppingList[index].ingrediant + '</li>');
     }
+    $('body').append('<hr>');
 }
 
 // this function can stay the same even when we
 // are connecting to real API
 function getAndDisplayShoppingList() {
-    console.log("is this working");
     getShoppingList(displayShoppingList);
 }
+
+let MOCK_RECIPES = {
+    "recipes": [{
+            "id": "1111111",
+            "name": "milk shake",
+            "ingrediants": [{
+                    "amount": "2 tbs",
+                    "item": "Chocolate Syrup"
+                },
+                {
+                    "amount": "3 cups",
+                    "item": "milk"
+                },
+                {
+                    "amount": "1",
+                    "item": "Glass"
+                },
+            ],
+            "instructions": "list of steps"
+        },
+        {
+            "id": "2222222",
+            "name": "smoothie",
+            "ingrediants": [{
+                    "amount": "2 cups",
+                    "item": "strawberries"
+                },
+                {
+                    "amount": "3 cups",
+                    "item": "milk"
+                },
+                {
+                    "amount": "1",
+                    "item": "banana"
+                },
+                {
+                    "amount": "1",
+                    "item": "Glass"
+                },
+            ],
+            "instructions": "list of steps"
+        }
+    ]
+};
+
+function getRecipes(callbackFn) {
+    // we use a `setTimeout` to make this asynchronous
+    // as it would be with a real AJAX call.
+    setTimeout(function () {
+        callbackFn(MOCK_RECIPES)
+    }, 1);
+}
+
+
+// this function stays the same when we connect
+// to real API later
+function displayRecipes(data) {
+    console.log(data);
+    for (index in data.recipes) {
+        $('body').append(
+            '<ul>' + data.recipes[index].name + '</ul>');
+    }
+    $('body').append('<hr>');
+}
+
+// this function can stay the same even when we
+// are connecting to real API
+function getAndDisplayRecipes() {
+    getRecipes(displayRecipes);
+}
+
+
+
+
+
+
 
 // //  on page load do this
 $(function () {
     getAndDisplayShoppingList();
+    getAndDisplayRecipes();
 })
